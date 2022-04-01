7fd76c528e7b7e2737da2575f608a5e73c3e54c8
m-c-hill <matthewhill2595@gmail.com>
2022-04-01 20:45:09 +0100

    Simple authorization process

--- app.py
+++ app.py
@@ -1,21 +1,72 @@
 import json
+import os
+from urllib.parse import urlencode
 
 import requests
+from flask import Flask, redirect, request, Response, jsonify, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
-def main():
-    with open(".secrets/strava-secrets.json") as f:
-        credentials = json.load(f)
-    breakpoint()
-    token = credentials["access_token"]
-    headers = {"Authorization": f"Bearer {token}"}
+def read_secrets(secret_path):
+    try:
+        with open(secret_path) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"Secret not found in {secret_path}")
 
-    response = requests.get(
-        f"https://www.strava.com/api/v3/athlete", headers=headers
+
+def create_app() -> Flask:
+    app = Flask(__name__, template_folder='templates')
+    secret_config = read_secrets(".secrets/strava-secrets.json")
+    app.config.update(secret_config)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = f"sqlite:///{os.path.join(basedir, 'data.sqlite')}"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    return app
+
+
+app = create_app()
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/auth", methods=["GET"])
+def strava_authorize():
+    params = {
+        "client_id": app.config["STRAVA_CLIENT_ID"],
+        "response_type": "code",
+        "redirect_uri": app.config["REDIRECT_URI"],
+        "scope": "activity:read_all",
+    }
+    return redirect(f"https://www.strava.com/oauth/authorize?{urlencode(params)}")
+
+
+def exchange_token(code):
+    strava_request = requests.post(
+        'https://www.strava.com/oauth/token',
+        data={
+            'client_id': app.config["STRAVA_CLIENT_ID"],
+            'client_secret': app.config["STRAVA_CLIENT_SECRET"],
+            'code': code,
+            'grant_type': 'authorization_code'
+        }
     )
+    return jsonify(strava_request.json())
 
-    r.raise_for_status()
 
+@app.route("/token")
+def user_token_exchange():
+    """Receive the user code and query Strava to get the final access token."""
+    code = request.args.get('code')
+    if not code:
+        return Response('Error: Missing code param', status=400)
+    return exchange_token(code)
 
-if __name__ == "__main__":
-    main()
--- scratch.py
+++ scratch.py
@@ -1,16 +1,64 @@
-# Scratch file for testing simple code
+import json
+import os
+from pprint import pprint
 
-import asyncio
+import requests
+from flask import Flask
+from flask_cors import CORS
 
-async def main():
-    # Returns a co-routine object whose execution must be awaited
-    print("Matt")
-    task = asyncio.create_task(foo("Text"))
-    print("Finish")
+app = Flask(__name__)
+CORS(app)
 
-async def foo(text):
-    print(text)
-    await asyncio.sleep(1)
 
-# Event loop
-asyncio.run(main())
+def main():
+    with open(".secrets/strava-secrets.json") as f:
+        credentials = json.load(f)
+
+    # Get authorization code from the auth endpoint
+    CLIENT_ID = credentials["client_id"]
+    REDIRECT_URI = "http://localhost"
+    RESPONSE_TYPE = "code"
+    SCOPE = "activity:read"
+    headers = {
+        "client_id": CLIENT_ID,
+        "redirect_uri": REDIRECT_URI,
+        "response_type": RESPONSE_TYPE,
+        "scope": SCOPE,
+    }
+
+    # "Authorization": f"Bearer {token}"}
+
+    response = requests.post(
+        f"https://www.strava.com/api/v3/authorize", headers=headers
+    )
+    breakpoint()
+    token = credentials["access_token"]
+
+    # Athlete
+    print("Request athlete id: 79857")
+    get_response("athlete", headers)
+
+    # TODO: request read_all as scope for authentication
+
+    # Athlete Zones
+    print("Getting athlete zones...")
+    get_response("athlete/zones", headers)
+
+    # Activities
+    # activity_id = 6903475957
+    # print(f"Request activity id: {activity_id}")
+    # get_response(f"activities/{activity_id}", headers)
+
+
+def get_response(endpoint: str, headers: dict) -> None:
+    response = requests.get(
+        f"https://www.strava.com/api/v3/{endpoint}", headers=headers
+    )
+    response.raise_for_status()
+    response = response.json()
+    pprint(response)
+    print("\n")
+
+
+if __name__ == "__main__":
+    main()
